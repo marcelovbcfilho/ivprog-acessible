546fa8c8b79844d922d99c66553b06cb80bc3343
Marcelo Vilas Boas Correa Filho <marcelovbcfilho@gmail.com>
2020-08-17 10:13:52 -0300

    fix: started debugging keyboard press and fixed compiler issues

--- public/javascripts/compiler.js
+++ public/javascripts/compiler.js
@@ -1,10 +1,16 @@
 function compile() {
     let operations = document.getElementsByName('mainOperationsType');
     let stringToBeCompiled = ``;
+    let compiledJavascriptCode = $("#compiledJavascriptCode");
+
+    // Cleaning compiled code area
+    compiledJavascriptCode.html(``);
 
     // declaring variables
+    compiledJavascriptCode.append(`//Declarando variáveis<br/>`);
     for (let i = 0; i < variables.length; i++) {
-        stringToBeCompiled += `let ${variables[i].name} = ${variables[i].value};`;
+        stringToBeCompiled += `let ${variables[i].name} = ${getVariableTypeBarrier(variables[i].type)}${variables[i].value}${getVariableTypeBarrier(variables[i].type)};\n`;
+        compiledJavascriptCode.append(`let ${variables[i].name} = ${getVariableTypeBarrier(variables[i].type)}${variables[i].value}${getVariableTypeBarrier(variables[i].type)}; <br/>`);
     }
 
     for (let i = 0; i < operations.length; i++) {
@@ -12,33 +18,40 @@ function compile() {
             case mainCodeTypes.attribution: {
                 console.log(operations[i].previousElementSibling.value);
 
-                if (i !== 0)
+                if (i !== 0) {
                     stringToBeCompiled += `;`;
+                }
 
                 stringToBeCompiled += `\n${getVariableById(operations[i].previousElementSibling.value).name} = `;
+                compiledJavascriptCode.append(`<br/>${getVariableById(operations[i].previousElementSibling.value).name} = `);
                 break;
             }
             case mainCodeTypes.variable: {
                 console.log(operations[i].previousElementSibling.value);
                 stringToBeCompiled += `${getVariableById(operations[i].previousElementSibling.value).name} `;
+                compiledJavascriptCode.append(`${getVariableById(operations[i].previousElementSibling.value).name} `);
                 break;
             }
             case mainCodeTypes.operation: {
                 let operation = getOperationById(operations[i].previousElementSibling.value);
                 console.log(`${operation.firstVariable.name} ${operation.operation.operator} ${operation.secondVariable.name}`);
                 stringToBeCompiled += `${operation.firstVariable.name} ${operation.operation.operator} ${operation.secondVariable.name} `;
+                compiledJavascriptCode.append(`${operation.firstVariable.name} ${operation.operation.operator} ${operation.secondVariable.name} `);
                 break;
             }
             case mainCodeTypes.operator: {
                 console.log(operations[i].previousElementSibling.value);
                 stringToBeCompiled += `${getOperationByValue(operations[i].previousElementSibling.value).operator} `;
+                compiledJavascriptCode.append(`${getOperationByValue(operations[i].previousElementSibling.value).operator} `);
                 break;
             }
         }
     }
 
+    compiledJavascriptCode.append(`<br/><br/>//Exibindo variáveis`);
     for (let i = 0; i < variables.length; i++) {
         stringToBeCompiled += `\nalert("${variables[i].name} = " + ${variables[i].name});`;
+        compiledJavascriptCode.append(`<br/>alert("${variables[i].name} = " + ${variables[i].name});`);
     }
 
     eval(stringToBeCompiled);
